eb7e25877865d930170cf6d29eedf3a8e19067e3
Juan Wolfaardt <juanwolfaardt@gmail.com>
2024-05-13 15:47:20 +0200

    feat: add loading more coins when scrolling to the bottom of the list

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -2,7 +2,14 @@
 
 import ListItem, { ListItemSelectFunc } from "@components/list-item";
 import Loader from "@components/loader";
-import { ReactNode, useEffect, useState } from "react";
+import {
+  ReactElement,
+  ReactNode,
+  UIEvent,
+  UIEventHandler,
+  useEffect,
+  useState,
+} from "react";
 import useSWR from "swr";
 import styles from "./styles.module.css";
 
@@ -25,13 +32,24 @@ interface CoinMarketResponse {
 const LIST_ITEM_FADE_MS_MULTIPLIER: number = 125;
 
 const List = (props: ListProps): ReactNode => {
+  const [itemsPerPage, setItemsPerPage] = useState<number>(10);
+  const [page, setPage] = useState<number>(1);
   const [haveLoader, setHaveLoader] = useState<boolean>(true);
   const [errorMessage, setErrorMessage] = useState<string>("");
-  const [listItems, setListItems] = useState<ReactNode[]>([]);
+  const [listItems, setListItems] = useState<ReactElement[]>([]);
+  const [bottomReached, setBottomReached] = useState<boolean>(false);
 
   const { isLoading, error, data } = useSWR<CoinMarketResponse[]>(
-    `/coins/markets?vs_currency=${props.currency}&per_page=10&price_change_percentage=7d&precision=2`,
+    `/coins/markets?vs_currency=${props.currency}&per_page=${itemsPerPage}&page=${page}&price_change_percentage=7d&precision=2`,
   );
+
+  useEffect(() => {
+    setBottomReached(true);
+    setListItems([]);
+    setHaveLoader(true);
+    setPage(1);
+    setItemsPerPage(10);
+  }, [props.currency]);
 
   useEffect((): void => {
     if (isLoading) {
@@ -54,10 +72,10 @@ const List = (props: ListProps): ReactNode => {
       return;
     }
 
-    const listItemComponents: ReactNode[] = data.map<ReactNode>(
+    const listItemComponents: ReactElement[] = data.map<ReactElement>(
       (entry: CoinMarketResponse, index: number) => (
         <ListItem
-          key={`list-item-${index}`}
+          key={`list-item-${index}-${entry.id}`}
           fadeInDelay={`${index * LIST_ITEM_FADE_MS_MULTIPLIER}ms`}
           coinId={entry.id}
           logoURL={entry.image}
@@ -75,20 +93,63 @@ const List = (props: ListProps): ReactNode => {
       ),
     );
 
-    setListItems(listItemComponents);
-  }, [isLoading, error, data, props.currency, props.onListItemSelect]);
+    setListItems((current: ReactElement[]): ReactElement[] => {
+      if (current.length === 10 && listItemComponents.length === 100) {
+        const result: ReactElement[] = listItemComponents.filter(
+          (item: ReactElement, index: number): boolean => {
+            if (index < current.length) {
+              return item.key !== current[index].key;
+            }
+
+            return true;
+          },
+        );
+
+        return [...current, ...result];
+      }
+
+      return [...current, ...listItemComponents];
+    });
+
+    setBottomReached(false);
+    // Doing this because list is being extended with the last loaded values each time a list item is selected or the currency is changed
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading, error, data]);
+
+  const scrollHandler: UIEventHandler<HTMLDivElement> = (
+    event: UIEvent<HTMLDivElement>,
+  ): void => {
+    if (
+      event.currentTarget.clientHeight + event.currentTarget.scrollTop ===
+      event.currentTarget.scrollHeight
+    ) {
+      if (!bottomReached) {
+        setBottomReached(true);
+
+        if (itemsPerPage === 10) {
+          setItemsPerPage(100);
+        } else {
+          setPage(page + 1);
+        }
+      }
+    }
+  };
 
   return (
     <div className={styles.container}>
       {haveLoader ? (
         <div
-          className={`${styles.loader}${isLoading ? "" : ` ${styles["loader-fade-out"]}`}`}
+          className={`${styles.loader}${isLoading ? ` ${styles["loader-fade-in"]}` : ` ${styles["loader-fade-out"]}`}`}
         >
           <Loader />
         </div>
       ) : (
         (errorMessage && <div className={styles.error}>{errorMessage}</div>) ||
-        (listItems && <div className={styles.list}>{listItems}</div>)
+        (listItems && (
+          <div className={styles.list} onScroll={scrollHandler}>
+            {listItems}
+          </div>
+        ))
       )}
     </div>
   );
